8090f47c7f217cdef397064c3979b80b5931d8f8
Alexandre Normand <alexandre.normand@gmail.com>
2014-11-11 21:10:43 -0800

    Port to Heroku-compatible App.

--- phoenix/main.go
+++ phoenix/main.go
@@ -5,34 +5,27 @@ import (
 	// an algorithm that isn't available by default (sha384)
 	// https://groups.google.com/forum/#!topic/Golang-nuts/hqm_ssUNUtI for details
 	_ "crypto/sha512"
-	"flag"
-	"strings"
-
 	"github.com/Sirupsen/logrus"
 	"github.com/ehazlett/phoenix/plugins"
+
+	"os"
+	"strings"
 )
 
 var (
-	pluginStr  string
 	pluginList []string
 	listenAddr string
 	logger     = logrus.New()
 	version    = "0.1"
 )
 
-func init() {
-	flag.StringVar(&pluginStr, "plugins", "", "list of enabled plugins (comma separated)")
-	flag.StringVar(&listenAddr, "listen", ":8080", "listen address")
-	flag.Parse()
-}
-
 func main() {
 	logger.Infof("phoenix v%s", version)
-	pluginList = strings.Split(pluginStr, ",")
+	pluginList = strings.Split(os.Getenv("PLUGINS"), ",")
 	logger.WithFields(logrus.Fields{
 		"names": pluginList,
 	}).Info("enabling plugins")
 	manager := plugins.New(pluginList)
-	server := NewServer(manager, listenAddr)
+	server := NewServer(manager, ":"+os.Getenv("PORT"))
 	server.Run()
 }
